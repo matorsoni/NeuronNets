bb77b899f0d41a11b2eae8f0bcde29d96c2820ea
matorsoni <matorsoni45@gmail.com>
2018-09-03 16:46:51 +0200

    -

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # NeuronNets
 
-Implementation of a feed forward NN for the classic MNIST classification and an implementation of a LSTM for time series analysis. All implementations use numpy.
+Implementation of a feed forward NN for the classic MNIST classification and an implementation of a LSTM for time series analysis.
+All implementations use numpy.
 
+## LSTM
+Implementation of a LSTM with peepholes connection, see the references folder.
+		
+The LSTM network may have many lstm blocks like the one shown in lstm/references/lstm_block_structure.png, all 
+stacked one on top of each other, though this implementation only allows one block networks.
 Getting the MNIST dataset: https://github.com/sorki/python-mnist
--- lstm/lstm.py
+++ lstm/lstm.py
@@ -4,7 +4,10 @@ from lstm_block import *
 class LSTM:
 	def __init__(self, inp_size: int, out_size: int, y_activation = 'sigmoid'):
 		'''
-		Simplest version of a LSTM (with peepholes), see figures at https://medium.com/@shiyan/materials-to-understand-lstm-34387d6454c1
+		
+		
+		
+		
 		T = total number of timestamps 
 		'''
 		self.inp_size = inp_size
@@ -12,10 +15,6 @@ class LSTM:
 		self.block = LSTM_Block(1, inp_size, out_size)
 		self.Y, self.d_Y = choose(y_activation)
 		
-		## Se pá nem precisa dessas paradas... os gates já tem weighted inputs no lstm_block
-		#self.W_ih = np.random.randn(inp_size, inp_size) # weighted input x_t
-		#self.W_hh = np.random.randn(inp_size, inp_size) # weighted recurrent input h_t_
-		#self.b_h = np.random.randn(inp_size, 1) # biased recurrent input
 		self.w_hy = np.random.randn(out_size, out_size) # weighted output y_t
 		self.b_y = np.random.randn(out_size, 1) # biased output y_t
 	
